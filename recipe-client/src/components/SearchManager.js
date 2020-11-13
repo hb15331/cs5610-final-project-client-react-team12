c99291dc78b277c37de89334da061035b34a2e5d
Mayuri Bedekar <mayuri.bedekar@gmail.com>
2020-11-13 12:13:34 -0800

    Added basic Home Page

--- recipe-client/src/components/SearchManager.js
+++ recipe-client/src/components/SearchManager.js
@@ -1,23 +1,46 @@
 import React from "react";
 import {BrowserRouter, Route, Link} from "react-router-dom";
 
+import HomePage from "./HomePage"
+import RegisterPage from "./RegisterPage";
+import ProfilePage from "./ProfilePage"
 import RecipeDetails from "./RecipeDetails";
 import SearchRecipe from "./SearchRecipe";
 
 export class SearchManager extends React.Component {
 
     render() {
-        return (
-            <BrowserRouter>
-                <div className="container">
-                    <Link to="/recipeSearch">
-                       Search Recipe
-                    </Link>
+            return (
+                <div>
+                    <BrowserRouter>
+                        <div className="container">
+                            <Link to="/home">
+                                Home
+                            </Link> |
+                            <Link to="/register">
+                                Sign Up
+                            </Link> |
+                            <Link to="/login">
+                                Log In
+                            </Link> |
+                            <Link to="/profile">
+                                Profile
+                            </Link> |
+                            <Link to="/recipeSearch">
+                                Search Recipe
+                            </Link>
 
-                    <Route path="/recipeSearch" component={SearchRecipe}/>
-                    <Route path="/recipes/:recipeUri" component={RecipeDetails}/>
+                            <Route path="/home" component={HomePage}/>
+                            <Route path="/register" component={RegisterPage}/>
+                            {/*    <Route path="/login" component={LoginPage}/> */}
+                            <Route path="/profile" component={ProfilePage}/>
+                            <Route path="/recipeSearch" component={SearchRecipe}/>
+                            <Route path="/recipes/:recipeUri" component={RecipeDetails}/>
+
+                        </div>
+                    </BrowserRouter>
                 </div>
-            </BrowserRouter>
-        );
+
+            );
+        }
     }
-}
