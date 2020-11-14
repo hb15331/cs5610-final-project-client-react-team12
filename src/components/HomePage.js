6dbf861a570d9fe4b2eb9277ef5b647157099bc0
Mayuri Bedekar <mayuri.bedekar@gmail.com>
2020-11-13 23:48:30 -0800

    Updated routing, restyled home page but needs more work

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,9 +4,9 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import SearchRecipe from "./SearchRecipe";
 import "../styling/HomePageStyle.css"
 
-// const APP_ID = "e488ff8f"
-// const APP_KEY = "922801bd953e0343123e19348ba693fe"
-// const RECIPE_URL = "https://api.edamam.com/search"
+const APP_ID = "e488ff8f"
+const APP_KEY = "922801bd953e0343123e19348ba693fe"
+const RECIPE_URL = "https://api.edamam.com/search"
 
 class HomePage extends React.Component {
 
@@ -16,8 +16,63 @@ class HomePage extends React.Component {
 
     state = {
         //imageUrl: "https://picsum.photos/200"
-        imageUrl: "https://image.shutterstock.com/image-photo/healthy-food-clean-eating-selection-600w-722718097.jpg"
+        imageUrl: "https://image.shutterstock.com/image-photo/healthy-food-clean-eating-selection-600w-722718097.jpg",
+        cuisineTypes: ["American", "Asian", "British", "Caribbean", "Central Europe", "Chinese", "Eastern Europe",
+            "French", "Indian", "Italian", "Japanese", "Kosher", "Mediterranean", "Mexican", "Middle Eastern", "Nordic",
+            "South American", "South East Asian"],
+        ingredients: ["chicken", "pork", "tomatoes", "cheese", "flour", "milk", "potato", "onion", "avocado", "beans",
+        "pasta", "pepper", "nuts", "beef", "vegetables", "bread", "egg", "fish"],
+        recipeOfTheDay: "",
+        rawRecipes: [],
+        recipeUri: "",
+        random: 0
     }
+
+
+    componentDidMount() {
+        {this.randomGenerator()}
+        {this.searchRecipes()}
+    }
+    
+
+    min = 1;
+    max = 18;
+
+    randomGenerator = () => {
+        this.setState({random: this.min + (Math.random() * (this.max - this.min))});
+        //this.setState({random: 5})
+    };
+
+
+
+    // fetch a list of recipes that match user's search criteria
+    searchRecipes = () => {
+        //console.log(this.state.keyword)
+        let ingredient = this.state.ingredients[this.state.random]
+        //let cuisineType = this.state.cuisineTypes[2]
+        //console.log(ingredient, cuisineType)
+        const queryUrl = `${RECIPE_URL}?q=${ingredient}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=1`
+        //const queryUrl =
+        // `${RECIPE_URL}?q=${this.state.keyword}&app_id=${APP_ID}&app_key=${APP_KEY}&cuisineType="indian"&from=0&to=1`
+        fetch(queryUrl)
+            .then(response => response.json())
+            .then((data) => this.renderRecipes(data))
+        //.then(this.renderRecipes)
+
+    }
+
+
+    // data includes all the info retrieved from api
+    renderRecipes = (data) =>
+        this.setState(prevState => ({
+            //...prevState,
+            // hits is an array of objects that include the true recipes we want
+            rawRecipes: data.hits
+
+        }))
+    //console.log(data)
+
+
 
     render() {
         return (
@@ -39,7 +94,43 @@ class HomePage extends React.Component {
 
                 </div>
 
-                <SearchRecipe/>
+                <div className="row">
+                    <div className="col-6">
+                        <h1>Recipe of the Day</h1>
+                        {
+                            this.state.rawRecipes.map(
+                                (rawRecipe, index) => {
+                                    // extract uri from data and use it as the unique identifier of recipe
+                                    const recipeUri = encodeURIComponent(rawRecipe.recipe.uri)
+                                    // console.log(recipeUri)
+
+                                    return (
+                                        <div key={index}>
+                                            {/*<Link to={`/recipes/${rawRecipe.recipe.label}`}>*/}
+                                            <Link to={`/recipes/${recipeUri}`}>
+
+                                                <h3>{rawRecipe.recipe.label}</h3>
+
+                                                <li className="container">
+                                                    {/*{JSON.stringify(rawRecipe)}*/}
+                                                    <img src={rawRecipe.recipe.image}/>
+                                                </li>
+
+                                            </Link>
+                                        </div>
+                                    )
+                                }
+                            )
+
+                        }
+
+                    </div>
+                    <div className="col-6">
+                        <SearchRecipe/>
+                    </div>
+
+                </div>
+
 
             </div>
 
