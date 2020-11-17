bfff85e57e0114ebdf87babef15ec611298e9b67
mmong <ong.ma@husky.neu.edu>
2020-11-16 22:53:53 -0500

    merged hualin's and mayuri's edits

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,19 +1,10 @@
 import React from 'react'
 import {Link} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"
+import RegisterPage from "./RegisterPage";
 
-// const APP_ID = "e488ff8f"
-// const APP_KEY = "922801bd953e0343123e19348ba693fe"
-// const RECIPE_URL = "https://api.edamam.com/search"
 
-class RegisterPage extends React.Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
+const ProfilePage = () =>
             <div>
 
 
@@ -44,15 +35,70 @@ class RegisterPage extends React.Component {
                 <ul id="orderlist">
 
                 </ul>
-
-
-
+                <Link to={`/home`} className="btn btn-success btn-block">
+                    Save
+                </Link>
 
             </div>
 
-        )
-    }
-}
+export default ProfilePage;
 
 
-export default RegisterPage;
+// import React from 'react'
+// import {Link} from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.min.css"
+//
+// // const APP_ID = "e488ff8f"
+// // const APP_KEY = "922801bd953e0343123e19348ba693fe"
+// // const RECIPE_URL = "https://api.edamam.com/search"
+//
+// class RegisterPage extends React.Component {
+//
+//     constructor(props) {
+//         super(props);
+//     }
+//
+//     render() {
+//         return (
+//             <div>
+//
+//
+//                 <h5>Profile</h5>
+//                 <span //TODO: add edit buttons
+//                 >
+//
+//                 <label for = "name">Name</label>
+//                 <input className="form-control" type="text" id="name"
+//                     //TODO: onchange input username
+//                 />
+//
+//                 </span>
+//                 <label for="name">Email</label>
+//                 <input className="form-control" type="email" id="name"
+//                     //TODO: onchange input username
+//                 />
+//                 <label for="telephone">Telephone</label>
+//                 <input className="form-control" type="tel" id="telephone"
+//                     //TODO: onchange input password
+//                 />
+//                 <label for="location">Location</label>
+//                 <input className="form-control" type="text" id="location"
+//                     //TODO: onchange input password
+//                 />
+//
+//                 <label for="orderlist">Orders:</label>
+//                 <ul id="orderlist">
+//
+//                 </ul>
+//
+//
+//
+//
+//             </div>
+//
+//         )
+//     }
+// }
+//
+//
+// export default RegisterPage;
